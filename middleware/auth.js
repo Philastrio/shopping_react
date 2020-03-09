72dec91ac596519e9ee9b68799cea30f7e51e9ff
philastrio <han_gang@naver.com>
2020-03-10 05:10:02 +0900

    auth token

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+
+  // Check for token
+  if (!token) {
+    res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ msg: "Token is not valid" });
+  }
+};
+
+export default auth;
